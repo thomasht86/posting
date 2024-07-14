0ca22f77df12ec5091bdb407f73ea144090c7678
thomasht86 <thomas.h.thoresen@gmail.com>
2024-07-14 13:23:44 +0200

    fix import on win

--- src/posting/vespa.py
+++ src/posting/vespa.py
@@ -1,24 +1,15 @@
 from textual.containers import Vertical
 from textual.widgets import Label, Markdown, Button
-from textual.app import ComposeResult, on  
+from textual.app import ComposeResult, on
 from textual.message import Message
-from posting.widgets.request.url_bar import SendRequestButton
-from textual.widgets import Input, Markdown, Placeholder, Label, ContentSwitcher, Button, Collapsible
-from textual.app import ComposeResult
-from textual.containers import VerticalScroll, Horizontal, Vertical, Center
-from textual.binding import Binding
-from textual import work, on
+from textual.widgets import Input, ContentSwitcher
+from textual.containers import VerticalScroll, Horizontal, Center
+from textual import work
 from textual.reactive import reactive
-from textual.message import Message
-from pydantic import BaseModel, ConfigDict
 from pydantic.dataclasses import dataclass as pydantic_dataclass
 from posting.widgets.vespa.buttons import SearchButton, FilterButton
 from dataclasses import dataclass
 import httpx
-import asyncio
-from dataclasses import dataclass
-from subprocess import run
-import pty
 import os
 import sys
 import subprocess
@@ -27,23 +18,24 @@ import select
 from typing import List
 
 
-
 class VespaPage(Vertical):
-    """ The Vespa page. """
+    """The Vespa page."""
 
     @dataclass
     class AuthenticatedMessage(Message):
         authenticated: bool = True
-    
+
     def compose(self) -> ComposeResult:
         yield Label("Vespa")
         yield Button("Authenticate", id="auth_button", variant="primary")
         yield Markdown("Output:", id="output")
-    
+
     @on(Button.Pressed, "#auth_button")
     def run_vespa_auth_login(self, event):
         # Run the Vespa auth login command and capture the output
         # Open a new pseudo-terminal
+        import pty
+
         master, slave = pty.openpty()
 
         # Start the subprocess with its input/output connected to the PTY
@@ -69,7 +61,9 @@ class VespaPage(Vertical):
                         output = os.read(master, 1024).decode("utf-8")
                         if output:
                             print(output, end="")
-                            self.query_one("#output", Markdown).update(markdown=f"Output: {output}")
+                            self.query_one("#output", Markdown).update(
+                                markdown=f"Output: {output}"
+                            )
                             sys.stdout.flush()
                         if "Success:" in output:
                             finished = True  # Exit the loop after success message
@@ -77,7 +71,7 @@ class VespaPage(Vertical):
 
                         # Check for input only if running in a Jupyter Notebook
                         if "[Y/n]" in output:
-                            user_input = "y\n" #input() + "\n"
+                            user_input = "y\n"  # input() + "\n"
                             os.write(master, user_input.encode())
                             sys.stdout.flush()
                 if finished:
@@ -92,27 +86,28 @@ class VespaPage(Vertical):
         # Add the output to the Markdown widget
         msg = self.AuthenticatedMessage(authenticated=True)
         posted = self.post_message(msg)
-        self.query_one("#output", Markdown).update(markdown=f"Output: {posted} Msg: {msg}")
-
-
-
-
+        self.query_one("#output", Markdown).update(
+            markdown=f"Output: {posted} Msg: {msg}"
+        )
 
 
 @dataclass
 class ChatResponse(Message):
     response: str
 
+
 @dataclass
 class ResetChat(Message):
     pass
+
 
 @dataclass
 class ResetSearch(Message):
     pass
 
+
 @pydantic_dataclass(config=dict(extra="allow"))
-class SearchResult():
+class SearchResult:
     number: int
     title: str
     base_uri: str
@@ -124,9 +119,11 @@ class SearchResult():
     def to_markdown(self) -> str:
         return f"### [{self.number}] **{self.title}**\n___\n{self.content}\n\n[Read more]({self.base_uri}{self.path})\n___"
 
+
 @dataclass
 class SearchResponse(Message):
     results: List[SearchResult]
+
 
 class DocSearchView(Horizontal):
     """DocSearchView"""
@@ -170,7 +167,13 @@ class DocSearchView(Horizontal):
     }
     """
 
-    FILTERS = {"docs": "+namespace:open-p", "cloud-docs": "+namespace:cloud-p", "sample-apps":"+namespace:vespaapps-p", "blog": "+namespace:blog-p", "pyvespa": "+namespace:pyvespa-p"}
+    FILTERS = {
+        "docs": "+namespace:open-p",
+        "cloud-docs": "+namespace:cloud-p",
+        "sample-apps": "+namespace:vespaapps-p",
+        "blog": "+namespace:blog-p",
+        "pyvespa": "+namespace:pyvespa-p",
+    }
     # Initialize the button states to False
     BUTTON_STATES = {filter_name: True for filter_name in FILTERS.keys()}
 
@@ -178,13 +181,11 @@ class DocSearchView(Horizontal):
     has_searched: bool = False
 
     class AbstractMarkdown(Markdown):
-        
-
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
             self.init_text = ""
             self.text = reactive(self.init_text)
-        
+
         def reset_text(self) -> None:
             self.text = self.init_text
 
@@ -193,12 +194,13 @@ class DocSearchView(Horizontal):
 
         def watch_text(self) -> None:
             self.update(markdown=self.text)
-    
+
     def compose(self) -> ComposeResult:
         self.border_title = "Search vespa.ai documentation"
         self.add_class("section")
         self.styles.border_title_align = "center"
         with VerticalScroll() as search_column:
+            search_column.styles.background = "background-lighten-1"
             search_column.styles.width = "2fr"
             with Horizontal() as search_bar:
                 search_bar.styles.height = 1
@@ -211,12 +213,17 @@ class DocSearchView(Horizontal):
             with Horizontal() as filters:
                 filters.styles.margin = 1
                 filters.styles.height = 1
-                yield Label("Filter by:")
+                # yield Label("Filter by:")
                 for filter_name, filter_query in self.FILTERS.items():
-                    yield FilterButton(filter_name, id=filter_name, variant="success", classes="filter-button")
+                    yield FilterButton(
+                        filter_name,
+                        id=filter_name,
+                        variant="success",
+                        classes="filter-button",
+                    )
                     yield Label(" ")
             with VerticalScroll(id="all-search-results") as results_area:
-                yield Label("Empty for now", id="empty-results")    
+                yield Label("Empty for now", id="empty-results")
         with VerticalScroll() as chat_area:
             chat_area.styles.width = "1fr"
             chat_area.styles.height = "auto"
@@ -237,29 +244,45 @@ class DocSearchView(Horizontal):
         # TODO: Should only be when DocSearchView tab is selected.
         self.query_one(selector="#search-input").focus()
         # TODO: Seems like the reactivity of the markdown widget is not working
-        self.watch(self.query_one(selector="#chat-response"), "text", self.query_one(selector="#chat-response").watch_text)
+        self.watch(
+            self.query_one(selector="#chat-response"),
+            "text",
+            self.query_one(selector="#chat-response").watch_text,
+        )
         # Add binding to self.app
         # BINDINGS = [Binding("ctrl+enter", "send_request", "Search"),]
         # for binding in BINDINGS:
         #     self.app.BINDINGS.append(binding)
-    
+
     def get_filter_string(self) -> str:
         # TODO: The filters does not seem to be applied correctly
-        return " ".join([self.FILTERS[filter_name] for filter_name, is_active in self.BUTTON_STATES.items() if is_active])
+        return " ".join(
+            [
+                self.FILTERS[filter_name]
+                for filter_name, is_active in self.BUTTON_STATES.items()
+                if is_active
+            ]
+        )
 
     @work(exclusive=True)
     async def send_chat_via_worker(self) -> None:
-        await self.send_chat_request(query=self.search_query, filter_string=self.get_filter_string())
-    
+        await self.send_chat_request(
+            query=self.search_query, filter_string=self.get_filter_string()
+        )
+
     @work(exclusive=True)
     async def send_search_via_worker(self) -> None:
-        await self.send_search_request(query=self.search_query, filter_string=self.get_filter_string())
-    
+        await self.send_search_request(
+            query=self.search_query, filter_string=self.get_filter_string()
+        )
+
     @on(FilterButton.Pressed, selector=".filter-button")
     def handle_filter_button(self, event) -> None:
         # Get the current state of the button
         current_state = self.BUTTON_STATES[event.button.id]
-        self.query_one(selector=f"#{event.button.id}").set_class(not current_state, "disabled")
+        self.query_one(selector=f"#{event.button.id}").set_class(
+            not current_state, "disabled"
+        )
         self.BUTTON_STATES[event.button.id] = not current_state
 
     @on(Button.Pressed, selector="#chat-button")
@@ -269,7 +292,7 @@ class DocSearchView(Horizontal):
         self.abstract_consent = True
         if self.has_searched:
             self.send_chat_via_worker()
-    
+
     @on(Button.Pressed, selector="#search-button")
     def handle_search_via_event(self) -> None:
         """Send the request."""
@@ -283,7 +306,7 @@ class DocSearchView(Horizontal):
         self.abstract_markdown.add_text(event.response)
         self.abstract_markdown.update(markdown=self.abstract_markdown.text)
 
-    @on(message_type=ResetChat) 
+    @on(message_type=ResetChat)
     def reset_text(self) -> None:
         self.abstract_markdown.reset_text()
         self.abstract_markdown.update(markdown=self.abstract_markdown.text)
@@ -293,55 +316,59 @@ class DocSearchView(Horizontal):
         self.post_message(ResetChat())
 
         params = {
-            'query': query,
-            'filters': filter_string,
-            'queryProfile': self.query_profile,
+            "query": query,
+            "filters": filter_string,
+            "queryProfile": self.query_profile,
         }
 
         async with httpx.AsyncClient() as client:
             try:
-                response = await client.get(self.chat_url, headers=self.chat_request_headers, params=params)
+                response = await client.get(
+                    self.chat_url, headers=self.chat_request_headers, params=params
+                )
                 response.raise_for_status()
                 async for line in response.aiter_lines():
                     if line.startswith("data: "):
-                        data = line[len("data: "):]
+                        data = line[len("data: ") :]
                         self.post_message(ChatResponse(response=data))
                     elif line.startswith("event: end"):
                         print("\nEnd of message.")  # Final message post
                         break
             except httpx.HTTPStatusError as e:
                 raise e
-    
+
     async def send_search_request(self, query: str, filter_string: str) -> None:
         "Send search request"
 
-        #query = 'hello'
-        #filters = '+namespace:open-p +namespace:cloud-p +namespace:vespaapps-p +namespace:blog-p +namespace:pyvespa-p'
-        #query_profile = 'llmsearch'
+        # query = 'hello'
+        # filters = '+namespace:open-p +namespace:cloud-p +namespace:vespaapps-p +namespace:blog-p +namespace:pyvespa-p'
+        # query_profile = 'llmsearch'
 
         params = {
-            'query': query,
-            'filters': filter_string,
-            'queryProfile': self.query_profile,
+            "query": query,
+            "filters": filter_string,
+            "queryProfile": self.query_profile,
         }
         self.post_message(ResetSearch())
         async with httpx.AsyncClient() as client:
             try:
-                response = await client.get(self.search_url, headers=self.search_request_headers, params=params)
+                response = await client.get(
+                    self.search_url, headers=self.search_request_headers, params=params
+                )
                 response.raise_for_status()
                 resp = response.json()
-                children = resp.get('root', {}).get('children', []) 
+                children = resp.get("root", {}).get("children", [])
                 results = []
                 for number, child in enumerate(children, start=1):
-                    fields = child.get('fields', {})
+                    fields = child.get("fields", {})
                     result = SearchResult(
                         number=number,
-                        title=fields.get('title', ''),
-                        base_uri=fields.get('base_uri', ''),
-                        path=fields.get('path', ''),
-                        content=fields.get('content', ''),
-                        relevance=child.get('relevance', 0.0),
-                        source=child.get('source', '')
+                        title=fields.get("title", ""),
+                        base_uri=fields.get("base_uri", ""),
+                        path=fields.get("path", ""),
+                        content=fields.get("content", ""),
+                        relevance=child.get("relevance", 0.0),
+                        source=child.get("source", ""),
                     )
                     results.append(result)
                 for result in results:
@@ -351,20 +378,21 @@ class DocSearchView(Horizontal):
                 raise e
                 exit()
         self.post_message(search_response)
-        #self.query_one(selector="#empty-results").update(f"finished")
+        # self.query_one(selector="#empty-results").update(f"finished")
 
-    
     @on(message_type=SearchResponse)
     def on_search_response_received(self, event: SearchResponse) -> None:
-        mount_area = self.query_one(selector="#all-search-results", expect_type=VerticalScroll)
+        mount_area = self.query_one(
+            selector="#all-search-results", expect_type=VerticalScroll
+        )
         for result in event.results:
             md_result = result.to_markdown()
             mount_area.mount(Markdown(md_result))
-    
+
     @on(message_type=ResetSearch)
     def reset_search(self, event: ResetSearch) -> None:
         self.query_one(selector="#all-search-results").remove_children()
-        #self.query_one(selector="#empty-results").update("Empty for now")
+        # self.query_one(selector="#empty-results").update("Empty for now")
 
     @property
     def sample_response(self) -> SearchResponse:
@@ -386,60 +414,62 @@ class DocSearchView(Horizontal):
                 ),
             ]
         )
-    
+
     @property
     def search_request_headers(self) -> dict[str, str]:
         return {
-            'User-Agent': 'Vespa TUI',
-            'Accept': '*/*',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'application/json',
-            'Referer': 'https://search.vespa.ai/',
-            'Origin': 'https://search.vespa.ai',
-            'Connection': 'keep-alive',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-site',
-            'Priority': 'u=4',
+            "User-Agent": "Vespa TUI",
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "application/json",
+            "Referer": "https://search.vespa.ai/",
+            "Origin": "https://search.vespa.ai",
+            "Connection": "keep-alive",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "Priority": "u=4",
         }
-    
+
     @property
     def chat_request_headers(self) -> dict[str, str]:
         return {
-            'User-Agent': 'Vespa TUI',
-            'Accept': 'text/event-stream',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br, zstd',
-            'Referer': 'https://search.vespa.ai/',
-            'Origin': 'https://search.vespa.ai',
-            'Connection': 'keep-alive',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-site',
-            'Priority': 'u=1',
-            'TE': 'trailers',
+            "User-Agent": "Vespa TUI",
+            "Accept": "text/event-stream",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Referer": "https://search.vespa.ai/",
+            "Origin": "https://search.vespa.ai",
+            "Connection": "keep-alive",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "Priority": "u=1",
+            "TE": "trailers",
         }
-    
+
     @property
     def query_profile(self) -> str:
-        return 'llmsearch'
-    
+        return "llmsearch"
+
     @property
     def base_url(self) -> str:
-        return 'https://api.search.vespa.ai/'
+        return "https://api.search.vespa.ai/"
 
     @property
     def search_url(self) -> str:
-        return f'{self.base_url}search/'
-    
+        return f"{self.base_url}search/"
+
     @property
     def chat_url(self) -> str:
-        return f'{self.base_url}stream/'
-    
+        return f"{self.base_url}stream/"
+
     @property
     def search_query(self) -> str:
         return self.query_one(selector="#search-input").value
-    
+
     @property
     def abstract_markdown(self) -> Markdown:
-        return self.query_one(selector="#chat-response", expect_type=self.AbstractMarkdown)
+        return self.query_one(
+            selector="#chat-response", expect_type=self.AbstractMarkdown
+        )
